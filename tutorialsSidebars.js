d6ab9cbb7eb67c2f5576853c6e423229399cd74a
Alex Ishchenko <shock_31337@ukr.net>
2025-02-27 08:53:43 +0200

    Create tutorialsSidebars.js

---
+++ tutorialsSidebars.js
@@ -0,0 +1,25 @@
+// tutorialsSidebars.js
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const tutorialsSidebars = {
+  tutorials: [
+    {
+      type: 'doc',
+      id: 'intro',
+      label: 'Introduction',
+    },
+    {
+      type: 'doc',
+      id: 'install',
+      label: 'Installation',
+    },
+    {
+      type: 'doc',
+      id: 'multi-step-forms',
+      label: 'Multi-step Forms',
+    },
+    // Add more tutorials here as needed
+  ],
+};
+
+module.exports = tutorialsSidebars;
